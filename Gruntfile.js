8f987952fea703ba0318a8cbe38f14e90f6ce2af
jessica-jordan <jessica.jordan.fe.dev@gmail.com>
2016-04-15 14:30:49 +0100

    Revert "Merge pull request #14 from biojs/revert-pr-for-#12"
    This reverts commit 867f69aea016b1416fd7521d62080f06fa230137, reversing
    changes made to c2a5a5455144d1dfebdffc5e67e88d4902e6869c.

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,4 +18,5 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.registerTask('default', ['watch']);
+  grunt.registerTask('sassify',['sass']);
 }
